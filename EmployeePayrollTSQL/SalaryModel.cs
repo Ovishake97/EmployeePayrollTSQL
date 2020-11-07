0357014cc2984c44e4b8c560fcdaa2be7206fce5
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-11-07 20:50:25 +0530

    [Abhishek] added to the UC7 branch

---
+++ EmployeePayrollTSQL/SalaryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollTSQL
+{
+  public  class SalaryModel
+    {
+        public int SalaryId { get; set; }
+        public decimal basicPay { get; set; }
+        public decimal deduction { get; set; }
+        public decimal taxablePay { get; set; }
+        public decimal incomeTax { get; set; }
+        public decimal netPay { get; set; }
+    }
+}
